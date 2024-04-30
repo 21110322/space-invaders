c2e9b5dd7686f05fa0051697e0f94122b4f43a8e
21110322 <159966431+21110322@users.noreply.github.com>
2024-04-30 00:18:15 +0000

    Version 6

--- makefile
+++ makefile
@@ -1,6 +1,14 @@
+bin/test : src/tui_test.cpp
+	g++ src/tui_test.cpp -o bin/test -std=c++2a -lftxui-screen -lftxui-component -lftxui -Iinclude
+
 bin/space : src/main.cpp
 	g++ src/main.cpp -o bin/space -std=c++2a -lftxui-screen -lftxui-component -lftxui-dom -Iinclude     
 
+bin/ventana : src/ventana.cpp
+	g++ src/ventana.cpp -o bin/ventana -std=c++2a -lftxui-screen -lftxui-component -lftxui-dom .Iinclude
+	
 run : bin/space
 	./bin/space
-	
+
+runVentana : bin/ventana
+	./bin/ventana
--- src/ventana.cpp
+++ src/ventana.cpp
@@ -1,15 +1,41 @@
 #include <iostream>
+#include <string>
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
+#include <thread>
 using namespace std;
 using namespace ftxui;
 
-int main(int argc, char cont *argv[])
-{
-    auto Pantalla = Screen::Create(Dimension::Fixed(), Dimension::Fixed(10));
-    Pantalla.PixelAT(5.5).character = 'm';
-    Render(Pantalla, border(text("Hola")));
-    Pantalla.Print();
+int main(int argc, char const *argv[])
 
+{
+    string palabra="DVD";
+    int posicionInicialX = 0;
+    int posicionInicialY = 0;
+
+    auto Pantalla = Screen::Create(Dimension::Full(), Dimension::Full(10));
+   
+   while(true){
+this_thread::sleep_for(0.1s);
+
+int posicionPalabraX=0;
+int posicionPalabraY=0;
+
+for (auto&&letra : palabra)
+{
+    int posicionfinalX=posicionInicialX+posicionPalabraX;
+    int posicionfinalY=posicionInicialY+posicionPalabraY;
+
+    Pantalla.PixelAt(posicionfinalX, posicionfinalY).character = letra;
+    posicionPalabra++;
+}
+Pantalla.Print();
+Pantalla.Clear();
+
+cout<<Pantalla.ResetPosition();
+
+posicionInicialX++;
+posicionInicialY++;
+   }
     return 0;
 }
